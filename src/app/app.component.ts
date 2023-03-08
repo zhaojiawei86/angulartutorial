0c8e6cd2a301ee68861cadf8a2a14f22ffd77daf
zhaojiawei86 <59852184+zhaojiawei86@users.noreply.github.com>
2023-03-08 00:12:13 -0500

    update tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angulartutorial';
+  companyName = "ABC Company";
+
+  employee={
+    "id":1,
+    "name":"john",
+    "age":30,
+    "salary":1000
+  };
+
+  firstName = "John";
+  lastName = "Smith";
+
+
+  firstNum:number = 10;
+  secondNum:number = 20;
+  sum = 0;
+  addNumbers(){
+    console.log(this.firstNum);
+    console.log(this.secondNum);
+    this.sum = parseInt(this.firstNum.toString()) + this.secondNum;
+  };
+
+  isRedColor:any=false;
+
+  show = true;
+  toggleForm(){
+    this.show = !this.show;
+  };
+
+  employees=[
+    {"id":1, "name":"john", "age":30, "salary":1000},
+    {"id":2, "name":"smith", "age":30, "salary":1000},
+    {"id":3, "name":"rynn", "age":30, "salary":1000},
+    {"id":4, "name":"george", "age":30, "salary":1000}
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
